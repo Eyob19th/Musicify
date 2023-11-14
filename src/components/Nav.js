4b8c7b8ca3df37e5985f29e0ad2da79acfe9e7d9
Eyob Getachew <99553287+Eyob19th@users.noreply.github.com>
2023-11-14 20:43:21 +0300

    Add files via upload

---
+++ src/components/Nav.js
@@ -0,0 +1,36 @@
+import styles from "../styles/nav.module.css";
+import { FaRegUserCircle } from "react-icons/fa";
+import logo from "../Logos/Logo1.jpg";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <div>
+      <div className={styles.navBOX}>
+        <div className={styles.navImg}>
+          <a href="/">
+            <img src={logo} alt="" className={styles.logo} />
+          </a>
+        </div>
+
+        <div className={styles.navDetail}>
+          <div>
+            <Link to="/profile" className={styles.link}>
+              {" "}
+              <FaRegUserCircle className={styles.user} />
+            </Link>
+          </div>
+
+          <div>
+            <button>
+              <Link className={styles.link} to="/login">
+                Log Out
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Nav;
---
+++ src/components/TopArtists.js
@@ -0,0 +1,54 @@
+import millify from 'millify';
+import React from 'react';
+import styles from '../styles/topArtists.module.css';
+
+function TopArtists({ topArtists }) {
+  function openArtist(artistURL){
+    window.open(
+      artistURL,
+      '_blank'
+    );
+  }
+  return (
+    <div className={styles.topArtistsFrame}>
+      <h1 style={{
+        margin: " 0 20px"
+
+      }}>Top Artists</h1>
+      <div className={styles.topArtists}>
+        {
+          topArtists?.items?.map((item, ind) => (
+            <div className={styles.artistCard + " zoom"} onClick={()=>openArtist(item?.external_urls.spotify)}>
+              <img src={item?.images[0]?.url} className={styles.artistImage} />
+              <div className={styles.topGradient}>
+                <h2 style={{
+                  margin: "0 0 20px 0"
+                }}>{item.name}</h2>
+              </div>
+              <div className={styles.bottomGradient}>
+                <h3 style={{
+                  margin: "0 0 !important"
+                }}>{millify(item.followers.total)} Followers</h3>
+                <h3 style={{
+                  margin: "10px 0",
+                  display:"inline-block"
+                }}>
+                  {item.genres[0]}
+                </h3>
+                <h3 style={{
+                  margin: "10px 0",
+                  display:"inline-block",
+                  float:"right"
+                }}>
+                  &#128293;{item.popularity}
+                </h3>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default TopArtists;
